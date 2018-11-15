98559dacf0a563fea35ec689b9a162fa7d466b4a
nguyenduong <duongynguyen0501@gmail.com>
2018-11-15 16:05:34 +0700

    SC-56: add propTypes to check valid in props

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Products from './../components/Products';
 import ProductItem from './../components/ProductItem';
+import PropTypes from 'prop-types';
 
 class ProductsContainer extends Component {
   render() {
     let { products } = this.props;
     return (
       <Products>
-        { this.showProducts(products) }
+        {this.showProducts(products)}
       </Products>
     );
   }
@@ -22,6 +23,22 @@ class ProductsContainer extends Component {
     }
     return result;
   }
+}
+
+ProductsContainer.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        inventory: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired
+      }
+    )
+  ).isRequired
 }
 
 const mapStateToProps = (state) => ({
--- src/reducers/product.js
+++ src/reducers/product.js
@@ -2,23 +2,29 @@ const initialState = [
   {
     id: 1,
     name: 'Iphone XS Max',
+    image: 'https://techland.com.vn/asset/upload/san_pham/iphone/iphone-xs-xsmax-xr/xs-max/iphone-xs-max-gold.jpg',
     description: 'Sản phẩm do Apple sản xuất',
     price: 500,
-    inventory: 10
+    inventory: 10,
+    rating: 4
   },
   {
     id: 2,
     name: 'Galaxy Note 9',
+    image: 'https://i5.walmartimages.com/asr/979de7d5-ee91-44cb-9915-4852dde90948_1.adbd8f04fbaa5e88cf42f4933aae0437.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
     description: 'Sản phẩm do SamSung sản xuất',
     price: 300,
-    inventory: 2
+    inventory: 2,
+    rating: 3
   },
   {
     id: 3,
     name: 'Xiaomi Max 2',
+    image: 'https://www.viettablet.com/image/data/0-hinh-moi/xiaomi-mi-max-2-viettablet.png',
     description: 'Sản phẩm do Xiaomi sản xuất',
     price: 100,
-    inventory: 5
+    inventory: 5,
+    rating: 5
   },
 ]
 

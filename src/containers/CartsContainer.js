3390f8d0a0fee25cf4540fddf866e902f404dece
nguyenduong <duongynguyen0501@gmail.com>
2018-11-16 17:17:07 +0700

    SC-57: show cart item

--- src/containers/CartsContainer.js
+++ src/containers/CartsContainer.js
@@ -1,37 +1,49 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import Cart from "../components/Cart";
+import CartItem from "../components/CartItem";
+import * as Message from "../constants/Messages";
 
 class CartsContainer extends Component {
   render() {
     let { carts } = this.props;
-    return (
-      <div></div>
-    );
+    return <Cart>{this.showCartItem(carts)}</Cart>;
   }
+
+  showCartItem = arr => {
+    var result = Message.MSG_CART_EMPTY;
+    if (arr.length > 0) {
+      result = arr.map((item, index) => {
+        return <CartItem key={index} index={index} item={item} />;
+      });
+    }
+    return result;
+  };
 }
 
 CartsContainer.propTypes = {
   carts: PropTypes.arrayOf(
     PropTypes.shape({
-      product: PropTypes.shape(
-        {
-          id: PropTypes.number.isRequired,
-          name: PropTypes.string.isRequired,
-          image: PropTypes.string.isRequired,
-          description: PropTypes.string.isRequired,
-          price: PropTypes.number.isRequired,
-          inventory: PropTypes.number.isRequired,
-          rating: PropTypes.number.isRequired
-        },
-      ).isRequired,
+      product: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        inventory: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired
+      }).isRequired,
       quantity: PropTypes.number.isRequired
     })
   ).isRequired
-}
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   carts: state.cart
-})
+});
 
-export default connect(mapStateToProps, null)(CartsContainer);
+export default connect(
+  mapStateToProps,
+  null
+)(CartsContainer);
